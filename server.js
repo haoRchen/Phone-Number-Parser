fd79a38d9d2a8ddc1bb9b5b281d7c4941ff33984
haoC <hao.ran.chen24@gmail.com>
2018-01-23 21:10:09 -0500

    Web service init

---
+++ server.js
@@ -0,0 +1,20 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+
+let app = express()
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// define a simple route
+app.get('/', function(req, res){
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
+// listen for requests
+app.listen(3000, function(){
+    console.log("Server is listening on port 3000");
+});
