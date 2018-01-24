e2609c7466ea8a0387e3ec6e8dbb809f8984f1ad
haoC <hao.ran.chen24@gmail.com>
2018-01-24 10:33:33 -0500

    working phone number parsing

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 let express = require('express')
 let bodyParser = require('body-parser')
-let phoneUtil = require('google-libphonenumber').phoneUtil
-    , PNF = require('google-libphonenumber').PhoneNumberFormat
+let cors = require('cors')
+let phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance()
+let PNF = require('google-libphonenumber').PhoneNumberFormat
 let app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/api/phonenumbers/parse/text/:string', (req, res)=>{
-    let number = phoneUtil.parse(req.query)
-    res.json({"Phone number": `${phoneUtil.format(tel, PNF.NATIONAL)}`});
-});
+    // console.log(req.param('string'))
+    let string = req.param('string')
+    console.log(string)
+    let number = phoneUtil.parse(string,'US')    
+    // console.log(phoneUtil.format(number, PNF.NATIONAL))
+    res.json({"Phone number": phoneUtil.format(number, PNF.NATIONAL)})
+})
 app.get('/api/phonenumbers/parse/text/', (req, res)=>{
-    res.json({"Phone number": `none given`});
-});
+    res.json({"Phone number": `none given`})
+})
 app.post('/,')
 
 app.listen(3000, (err)=>{
     if (err) {
         return console.log('Failed to connect to port: ', err)
       }
-    console.log("Server is listening on port 3000");
-});
+    console.log("Server is listening on port 3000")
+})
