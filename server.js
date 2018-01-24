e767f2e77848d04d6c8024d8ff91e346f9011321
haoC <hao.ran.chen24@gmail.com>
2018-01-24 09:12:11 -0500

    route setup init

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 let express = require('express')
 let bodyParser = require('body-parser')
-
+let phoneUtil = require('google-libphonenumber').phoneUtil
+    , PNF = require('google-libphonenumber').PhoneNumberFormat
 let app = express()
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
-// define a simple route
-app.get('/', function(req, res){
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+app.get('/api/phonenumbers/parse/text/:string', (req, res)=>{
+    let number = phoneUtil.parse(req.query)
+    res.json({"Phone number": `${phoneUtil.format(tel, PNF.NATIONAL)}`});
 });
+app.get('/api/phonenumbers/parse/text/', (req, res)=>{
+    res.json({"Phone number": `none given`});
+});
+app.post('/,')
 
-// listen for requests
-app.listen(3000, function(){
+app.listen(3000, (err)=>{
+    if (err) {
+        return console.log('Failed to connect to port: ', err)
+      }
     console.log("Server is listening on port 3000");
 });
