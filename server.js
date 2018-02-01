749b0e88638d7a3badaad22db0983cd4935da21a
haoC <hao.ran.chen24@gmail.com>
2018-01-31 20:21:57 -0500

    need fixing for posting file.

--- server.js
+++ server.js
@@ -1,10 +1,71 @@
 const express = require('express')
 const port = process.env.PORT || 3000;
-const controller = require('./controller')
+const multer = require('multer')
+const bodyParser = require('body-parser')
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance()
+const PNF = require('google-libphonenumber').PhoneNumberFormat
+const fs = require('fs')
 
-const app = express()
+var app = express()
 
-app.use('/', controller)
+const upload = multer({dest: 'uploads/'})
+  
+app.use(bodyParser.urlencoded({ extended: true }))
+
+function ParseNumberFromString(string){
+    let numbers = []
+    let number
+    try{
+        string.split(',').forEach( str=>{
+        number = phoneUtil.parse(str, 'CA')
+        if(phoneUtil.isValidNumber(number)){
+            numbers.push(phoneUtil.format(number, PNF.NATIONAL))
+        }    
+    })
+    }catch(error){
+        // console.log(error)
+    }
+    return Array.from(new Set(numbers))
+}
+
+app.get('/', (req, res) => {
+    res.status(200).send('Default: Please go to the correct end point for parsing');
+})
+app.get('/api/phonenumbers/parse/text/:strings', (req, res)=>{
+    let parsedArr = []
+    parsedArr = ParseNumberFromString( req.params.strings)
+    if(req.params.strings == 'nothing' || req.params.strings == '' || parsedArr.length == 0){
+        res.status(400).send([])
+    }else{
+        res.status(200).json(parsedArr)
+    }
+   
+})
+app.get('/api/phonenumbers/parse/text/', (req, res)=>{
+    res.status(404).send([])
+})
+app.get('*', (req, res) => {
+    res.status(404).send([])
+  })
+app.post('*', function (req, res) {
+    res.status(404).send([])
+})
+app.post('/api/phonenumbers/parse/file', upload.single('file'), (req, res) =>{
+    if (!req.file) {
+        res.status(400).send('No file posted')
+    }else {
+        let finalArr = []
+        try{
+            var text = Buffer.from(fs.readFileSync(request.file.path).toString(), 'base64');
+            var texts = text.toString().split('\n')
+            finalArr = ParseNumberFromString(texts)   
+        }catch(error){
+            console.log(error)
+        }
+        res.setHeader('Content-Type', 'text/plain');
+        res.status(200).send(finalArr)
+    }
+})
 
 app.listen(port, (err)=>{
     if (err) {
---
+++ server.test.js
@@ -0,0 +1,40 @@
+const server = require('./server')
+const fs = require('fs')
+const request = require('supertest')
+
+const URL = 'http://localhost:3000'
+const postURL = '/api/phonenumbers/parse/file'
+const getValidUrl = '/api/phonenumbers/parse/text/Seneca%20Phone%20Number%3A%20416-491-5050%2C6478603041%2Csometexts%2C6478603041'
+const getInvalidUrl = '/api/phonenumbers/parse/text/'
+
+test('GET for empty URL should be: 404 and []', async () => {
+    const response = await request(URL).get(getInvalidUrl)
+    expect(response.statusCode).toBe(404)
+    expect(response.text).toBe('[]')
+})
+
+test('GET for URL with numbers should be 200 and [\"(416) 491-5050\",\"(647) 860-3041\"]', async () => {
+    const response = await request(URL).get(getValidUrl)
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('[\"(416) 491-5050\",\"(647) 860-3041\"]')
+})
+
+test('POST request for file containing number should be  [\"(416) 491-5050\",\"(647) 860-3041\"]', async () => {
+    const response = 
+        await request(URL)
+            .post(postURL)
+            .set('Content-Type', 'text/plain')
+            .attach('file', fs.readFileSync('./validTest.txt'), 'validTest.txt')
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('[\"(416) 491-5050\",\"(647) 860-3041\"]')
+})
+
+test('POST request for file without numbers should be 400 and \"[]\"', async () => {
+    const response = 
+        await request(URL)
+            .post(postURL)
+            .set('Content-Type', 'text/plain')
+            .attach('file', fs.readFileSync('./invalidTest.txt'), 'invalidTest.txt')
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('[]')
+})
